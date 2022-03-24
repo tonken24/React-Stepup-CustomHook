e60ecb748042ba2ae05890a7d6e3d9e4579c3f1a
tonken24 <91036805+tonken24@users.noreply.github.com>
2022-03-24 11:58:13 +0900

    custom hook finish custom hook finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,40 @@
 import "./styles.css";
 import { UserCard } from "./components/UserCard";
-import axios from "axios";
-import { User } from "./types/api/user";
-import { useState } from "react";
-import { UserProfile } from "./types/UserProfile";
+import { useAllUsers } from "./hooks/useAllUsers";
 
-const user = {
-  id: 1,
-  name: "yourname",
-  email: "1213@aaa.com",
-  address: "ADDRESS"
-};
+// const user = {
+//   id: 1,
+//   name: "yourname",
+//   email: "1213@aaa.com",
+//   address: "ADDRESS"
+// };
 //以下からサンプルデータを使用する。
 //https://jsonplaceholder.typicode.com/users
 
 export default function App() {
-  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
+  //カスタムフックを呼び出す。
+  //エラーなどのフラグは他の処理で同じフックを呼び出しても書き変わらない。
+  const { getUsers, userProfiles, loading, error } = useAllUsers();
 
   const onClickFetchUser = () => {
-    axios
-      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => {
-        const data = res.data.map((user) => ({
-          id: user.id,
-          name: `${user.name}(${user.username})`,
-          email: user.email,
-          address: `${user.address.city}${user.address.suite}${user.address.street}`
-        }));
-        setUserProfiles(data);
-      });
+    //カスタムフックの関数を呼び出す。
+    getUsers();
   };
   return (
     <div className="App">
       <button onClick={onClickFetchUser}>データ取得</button>
-      {userProfiles.map((user) => (
-        <UserCard key={user.id} user={user} />
-      ))}
+      <br />
+      {error ? (
+        <p style={{ color: "red" }}>データの取得に失敗しました。</p>
+      ) : loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          {userProfiles.map((user) => (
+            <UserCard key={user.id} user={user} />
+          ))}
+        </>
+      )}
       {/* <UserCard user={user} /> */}
     </div>
   );
