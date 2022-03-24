8b53125d5dae2ef8982bb1eab7ed58ffd259dfb2
tonken24 <91036805+tonken24@users.noreply.github.com>
2022-03-24 11:17:13 +0900

    showcard showcard

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,42 @@
 import "./styles.css";
+import { UserCard } from "./components/UserCard";
+import axios from "axios";
+import { User } from "./types/api/user";
+import { useState } from "react";
+import { UserProfile } from "./types/UserProfile";
+
+const user = {
+  id: 1,
+  name: "yourname",
+  email: "1213@aaa.com",
+  address: "ADDRESS"
+};
+//以下からサンプルデータを使用する。
+//https://jsonplaceholder.typicode.com/users
 
 export default function App() {
+  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
+
+  const onClickFetchUser = () => {
+    axios
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        const data = res.data.map((user) => ({
+          id: user.id,
+          name: `${user.name}(${user.username})`,
+          email: user.email,
+          address: `${user.address.city}${user.address.suite}${user.address.street}`
+        }));
+        setUserProfiles(data);
+      });
+  };
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <button onClick={onClickFetchUser}>データ取得</button>
+      {userProfiles.map((user) => (
+        <UserCard key={user.id} user={user} />
+      ))}
+      {/* <UserCard user={user} /> */}
     </div>
   );
 }
